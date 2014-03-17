b784beb666ea189f82cb3703af29b69106b2083b
Kevin Bjorke <kevin.bjorke+dev@gmail.com>
2014-03-17 08:00:21 -0700

    vectorized compute cost

--- mlclass-ex1-005/mlclass-ex1/computeCostMulti.m
+++ mlclass-ex1-005/mlclass-ex1/computeCostMulti.m
@@ -13,16 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-for i = 1:m
-	% h = theta(1)*X(i,1) + theta(2)*X(i,2);
-	h = theta' * X(i,:)'; % vectorized
-    J = J + (h-y(i))^2;
-end;
-J = J / (2*m);    
-
-
-
-
+p = X*theta - y;
+J = p' * p;
+J = J /(2*m);
 
 % =========================================================================
 
